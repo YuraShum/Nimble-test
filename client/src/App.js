3709175a1677a50be50f510c24e13986f251d8f3
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-08-02 16:05:08 +0300

    initialization of the project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import ContactList from "./components/ContactsList/ContactList";
+import './index.css'
+function App() {
+  return (
+    <div className="mx-auto max-w-[1280px] min-w-[400px] px-4 bg-cyan-500">
+      <ContactList/>
+    </div>
+  );
+}
+
+export default App;
