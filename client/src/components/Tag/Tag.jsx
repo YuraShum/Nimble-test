3d536477a7de65d916e689fe1bebc8f1b20c8516
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-08-02 17:47:17 +0300

    creating a repeating tag component

---
+++ client/src/components/Tag/Tag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tag = ({text}) => {
+  return (
+    <p className='bg-secondary-300 rounded px-3 text-sm text-center w-fit font-medium'>{text}</p>
+  )
+}
+
+export default Tag
